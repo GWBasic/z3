2498b40e1e9bfa1c348597c781f29480eac83167
Andrew Rondeau <andrewrondeau@Andrews-MacBook-Pro.local>
2020-05-31 16:07:29 -0400

    Attempt to use postgres events so that the blog doesnt need to poll. Doesn't work

--- dbConnector.js
+++ dbConnector.js
@@ -1,8 +1,10 @@
-const Pool = require('pg').Pool
+const { Client, Pool } = require('pg');
+const format = require('pg-format');
 
 const connectionString = process.env.DATABASE_URL;
 
 var pool = null;
+var clients = [];
 
 module.exports = {
     connect: async () => {
@@ -13,8 +15,35 @@ module.exports = {
         return await pool.connect();
     },
 
+    listen: async (channel, callback, done) => {
+        var client = new Client(connectionString);
+        await client.connect();
+        await client.query(`LISTEN ${format.ident(channel)}`);
+
+        client.on('notification', data => {
+            callback(data.payload);
+        });
+
+        client.on('end', done);
+
+        clients.push(client);
+    },
+
     end: async () => {
-        pool.end();
+        if (pool) {
+            pool.end();
+        }
+
         pool = null;
+
+        for (var client of clients) {
+            client.end();
+        }
+
+        clients = [];
     }
 };
+
+process.on("exit", () => {
+    module.exports.end();
+});
