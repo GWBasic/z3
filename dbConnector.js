49d83d9683d55ef5fc22a610baeea9c6c3639b04
Andrew Rondeau <git@andrewrondeau.com>
2021-03-21 17:08:29 -0400

    Attempted to support SSL on Heroku

--- dbConnector.js
+++ dbConnector.js
@@ -2,21 +2,32 @@ const { Client, Pool } = require('pg');
 const format = require('pg-format');
 
 const connectionString = process.env.DATABASE_URL;
+const disableSSL = process.env.DISABLE_SSL != null
 
 var pool = null;
 var clients = [];
 
+const connectionArguments = {
+    connectionString: connectionString
+}
+
+if (!disableSSL) {
+    connectionArguments.ssl = {
+        rejectUnauthorized: false
+    }
+}
+
 module.exports = {
     connectToPool: async () => {
         if (pool == null) {
-            pool = new Pool({connectionString});
+            pool = new Pool(connectionArguments);
         }
 
         return await pool.connect();
     },
 
     connect: async () => {
-        const client = new Client(connectionString);
+        const client = new Client(connectionArguments);
         await client.connect();
 
         return client;
